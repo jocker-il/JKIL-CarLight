90402e5e0971d9087753b57c21c2fa82121b269c
Optera <yousei.kyoko+github@gmail.com>
2019-08-17 22:35:19 +0200

    fixed single headlight

--- data.lua
+++ data.lua
@@ -1,11 +1,16 @@
 -- Copyright (c) 2019 Optera
 
+local scale = 2
+
 -- update all oriented lights (head light) for the "car" entity
 if data.raw["car"]["car"] and data.raw["car"]["car"].light then
   for k, v in pairs(data.raw["car"]["car"].light) do
     if v.type and v.type == "oriented" then
-      v.shift = {1.2, -28}
-      v.size = 4
+      if v.shift then
+        v.shift[1] = v.shift[1] * scale
+        v.shift[2] = v.shift[2] * scale
+      end
+      v.size = v.size * scale
     end
   end
 end
