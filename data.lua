870e47684943525c4303362c3bd739555ecb847c
Optera <yousei.kyoko+github@gmail.com>
2019-08-17 18:36:53 +0200

    Add files via upload

--- data.lua
+++ data.lua
@@ -1,1 +1,11 @@
-require("prototypes.car_0-17")
+-- Copyright (c) 2019 Optera
+
+-- update all oriented lights (head light) for the "car" entity
+if data.raw["car"]["car"] and data.raw["car"]["car"].light then
+  for k, v in pairs(data.raw["car"]["car"].light) do
+    if v.type and v.type == "oriented" then
+      v.shift = {1.2, -28}
+      v.size = 4
+    end
+  end
+end
