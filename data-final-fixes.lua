84c700e406854965274f5c0f21526a8aefc221e2
The JoCKeR (IL) <13031901+jocker-il@users.noreply.github.com>
2019-08-18 22:10:47 +0200

    Add files via upload

---
+++ data-final-fixes.lua
@@ -0,0 +1,37 @@
+data.raw.car['car'].light = 
+    {
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "extra-high",
+          flags = { "light" },
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {-1.2, -28},
+        size = 4,
+        intensity = 0.6,
+        color = {r = 0.92, g = 0.77, b = 0.3}
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "extra-high",
+          flags = { "light" },
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {1.2, -28},
+        size = 4,
+        intensity = 0.6,
+        color = {r = 0.92, g = 0.77, b = 0.3}
+      }
+    }
